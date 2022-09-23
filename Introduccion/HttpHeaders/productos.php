e6b37e69dc15a4efb69db12d1695261eb5c2f625
AlejandrosMiralles <alumir3453@ieselcaminas.org>
2022-09-23 11:52:37 +0200

    productos ligeramente mejorado

--- Introduccion/HttpHeaders/productos.php
+++ Introduccion/HttpHeaders/productos.php
@@ -1,6 +1,6 @@
 <?php
     $productos = ["1" => "Producto 1", "2" => "Producto 2", "3" => "Producto 3"];
-    $id = $_GET['id'];
+    $id = $_GET['id'] ?? '-1';
 
     if (array_key_exists($id, $productos)){
         echo $productos[$id];
