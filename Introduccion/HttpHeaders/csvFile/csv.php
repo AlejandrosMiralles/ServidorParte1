b0dec44cb76a6bf4a5b4d39ed2088bfd8afa9903
AlejandrosMiralles <alumir3453@ieselcaminas.org>
2022-09-28 09:24:08 +0200

    formulario terminado y csv.php mejorado

--- Introduccion/HttpHeaders/csvFile/csv.php
+++ Introduccion/HttpHeaders/csvFile/csv.php
@@ -4,4 +4,9 @@
     header('Content-Disposition: attachment; filename="productos.csv"');
 
     readfile('productos');
+
+    $productos = ["1" => "Producto 1", "2" => "Producto 2", "3" => "Producto 3"];
+    foreach ($productos as $id=> $productos){
+        echo "$id;$valor";
+    }
 ?>
--- Introduccion/formularios/registrarse.php
+++ Introduccion/formularios/registrarse.php
@@ -1,23 +1,80 @@
 <?php 
+
+    function moverArchivoAImagenes(array $archivoAMover){
+        $rutaTemporal = $archivoAMover['tmp_name'];
+        $rutaFinal = "./imagenes/" . $archivoAMover['name'];
+
+        move_uploaded_file($rutaTemporal, $rutaFinal);
+
+        return $rutaFinal;
+    }
+
     if(isset($_POST["submit"]) && $_SERVER["REQUEST_METHOD"] == "POST"){
         $errores = [];
-        $dominiosRegistrados = ["gmail.com", "ieselcaminas.org", "hootmail.com"];
-
+        
+        //Comprobar que se ha introducido un nombre
         if (empty($_POST["nombre"])){
             $errores[] = "El campo nombre es obligatorio. Porfavor, introduzcalo";
         }
 
         //Comprobar que el correo es válido
-        if (empty($_POST["correo"])){
-            $errores[] = "El campo correo es obligatorio y debe ser de algún dominio comunmente usado";
-        }
+        if (empty($_POST["correo"]) || ! filter_var($_POST["correo"], FILTER_VALIDATE_EMAIL)){
+            $errores[] = "El campo correo es obligatorio y ser un email válido";
+        } 
 
-        if (empty($_POST['educacion'])){
-            $errores[] = "No se ha introducido ningun nivel de educacion";
-        }
+        //No se comprueba el nivel de educacion porque con <select> nos aserguramos de que siempre se especifique una opcion
+    
 
-        if (! isset($_FILES['avatar'])){
-            
+        //Comprobar que se ha introducido alguna imagen
+        //Por alguna razón siempre devuelve un array $_Files['avatar'], incluso si no se han introducido archivos 
+        if (0 == $_FILES['avatar']['size']){
+            $errores[] = "No se ha introducido ningun avatar";
+        } 
+
+
+        if (empty($errores)){
+            $nombre = $_POST["nombre"];
+            $email = $_POST["correo"]; 
+            $educacion = $_POST['educacion'];
+            $avatar = $_FILES['avatar'];
+
+            $rutaAvatar = moverArchivoAImagenes($avatar);
+
+
+            echo "$nombre; $email; $educacion; ";
+            echo "Avatar: <img src=\"$rutaAvatar\" />";
+        } else {
+            print_r($errores);
         }
     }
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST" enctype="multipart/form-data">
+        Nombre: <input type="text" name="nombre"/> 
+        <br/>
+        Email: <input type="text" name="correo" />
+        <br/>
+        Educacion: <select name="educacion">
+            <option value="Secundaria"> ESO </option>
+            <option value="Bachillerato"> Bachillerato </option>
+            <option value="FP"> Formacion Profesional </option>
+            <option value="Universidad"> Universidad </option>
+            <option value="SinEstudios"> Sin estudios </option>
+        </select>
+        <br/>
+        Avatar: <input type="file" name="avatar" />
+        <br/>
+        <input type="submit" name="submit" value="enviar" />
+    </form>
+
+</body>
+</html>
